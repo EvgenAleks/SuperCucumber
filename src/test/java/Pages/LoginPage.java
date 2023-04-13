3057fd843f80b196a44b35a03f94e9d998bc58bd
EvgenAleks <evgenii.aleksandroff@gmail.com>
2023-04-13 15:15:27 +0200

    13044

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -3,18 +3,30 @@ package Pages;
 import Utils.CommonMethods;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.bidi.log.Log;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage extends CommonMethods {
 
-    @FindBy(id="txtUsername")
+    public LoginPage() {
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(id = "txtUsername")
     public WebElement userNameTextBox;
 
-    @FindBy(id="txtPassword")
-    WebElement passWordTextBox;
+    @FindBy(id = "txtPassword")
+    public WebElement passWordTextBox;
 
-    @FindBy(id="btnLogin")
-    WebElement loginBtn;
+    @FindBy(id = "btnLogin")
+    public WebElement loginBtn;
+
+    @FindBy(id = "welcome")
+    public WebElement welcomeIcon;
+
+    @FindBy(xpath = "//a[text()='Logout']")
+    public WebElement logoutLink;
 
     // Page Factory Model
 
