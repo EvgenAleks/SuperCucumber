973d4055e8fa7fc6633f9abeaa3f4124799c4daa
EvgenAleks <evgenii.aleksandroff@gmail.com>
2023-04-13 15:02:15 +0200

    13043

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,5 +1,21 @@
 package Pages;
 
-public class LoginPage {
+import Utils.CommonMethods;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends CommonMethods {
+
+    @FindBy(id="txtUsername")
+    public WebElement userNameTextBox;
+
+    @FindBy(id="txtPassword")
+    WebElement passWordTextBox;
+
+    @FindBy(id="btnLogin")
+    WebElement loginBtn;
+
+    // Page Factory Model
 
 }
