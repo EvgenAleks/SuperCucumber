c73902e80fda5c6d589b4920799da13d99f43071
EvgenAleks <evgenii.aleksandroff@gmail.com>
2023-04-15 11:10:16 +0200

    15041

--- src/test/java/StepDefinitions/Login.java
+++ src/test/java/StepDefinitions/Login.java
@@ -1,5 +1,6 @@
 package StepDefinitions;
 
+import Pages.LoginPage;
 import Utils.CommonMethods;
 import Utils.ConfigReader;
 import io.cucumber.datatable.DataTable;
@@ -29,16 +30,22 @@ public class Login extends CommonMethods {
 
     @When("user enters valid email and valid password")
     public void user_enters_valid_email_and_valid_password() {
-        WebElement userNameTextBox = driver.findElement(By.id("txtUsername"));
-        sendText(userNameTextBox, ConfigReader.getPropertyValue("username"));
-        WebElement passWordTextBox = driver.findElement(By.id("txtPassword"));
-        sendText(passWordTextBox, ConfigReader.getPropertyValue("password"));
+
+        LoginPage login = new LoginPage();
+
+//        WebElement userNameTextBox = driver.findElement(By.id("txtUsername"));
+        sendText(login.userNameTextBox, ConfigReader.getPropertyValue("username"));
+//        WebElement passWordTextBox = driver.findElement(By.id("txtPassword"));
+        sendText(login.passWordTextBox, ConfigReader.getPropertyValue("password"));
     }
 
     @When("click on login button")
     public void click_on_login_button() {
-        WebElement loginBtn = driver.findElement(By.id("btnLogin"));
-        doClick(loginBtn);
+
+        LoginPage login = new LoginPage();
+
+//        WebElement loginBtn = driver.findElement(By.id("btnLogin"));
+        doClick(login.loginBtn);
     }
 
     @Then("user is logged in successfully")
@@ -51,10 +58,13 @@ public class Login extends CommonMethods {
 
     @When("user enters valid {string} and valid {string}")
     public void user_enters_valid_and_valid(String username, String password) {
-        WebElement userNameTextBox = driver.findElement(By.id("txtUsername"));
-        sendText(userNameTextBox, username);
-        WebElement passWordTextBox = driver.findElement(By.id("txtPassword"));
-        sendText(passWordTextBox, password);
+
+        LoginPage login = new LoginPage();
+
+//        WebElement userNameTextBox = driver.findElement(By.id("txtUsername"));
+        sendText(login.userNameTextBox, username);
+//        WebElement passWordTextBox = driver.findElement(By.id("txtPassword"));
+        sendText(login.passWordTextBox, password);
     }
 
 //    @Then("close the browser")
@@ -66,6 +76,8 @@ public class Login extends CommonMethods {
     @When("user enters valid username and password and verifies login")
     public void user_enters_valid_username_and_password_and_verifies_login(DataTable dataTable) {
 
+        LoginPage login = new LoginPage();
+
         List<Map<String, String>> userCredentials = dataTable.asMaps();
         for (Map<String, String> userCreds : userCredentials) {
             String username = userCreds.get("username");
@@ -74,6 +86,15 @@ public class Login extends CommonMethods {
             sendText(userNameTextBox, username);
             WebElement passWordTextBox = driver.findElement(By.id("txtPassword"));
             sendText(passWordTextBox, password);
+
+//            WebElement loginBtn = driver.findElement(By.id("btnLogin"));
+            doClick(login.loginBtn);
+
+//            WebElement welcomeIcon = driver.findElement(By.id("welcome"));
+            doClick(login.welcomeIcon);
+
+//            WebElement logoutLink = driver.findElement(By.xpath("//a[text()='Logout']"));
+            doClick(login.logoutLink);
         }
     }
 }
