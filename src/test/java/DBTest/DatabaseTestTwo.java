15592dc0d96762708ccb8180ecc52d203f1d1d24
EvgenAleks <evgenii.aleksandroff@gmail.com>
2023-05-16 01:07:03 +0200

    database 3

--- src/test/java/DBTest/DatabaseTestTwo.java
+++ src/test/java/DBTest/DatabaseTestTwo.java
@@ -19,20 +19,38 @@ public class DatabaseTestTwo {
             // create a statement to send sql queries
             Statement statement = conn.createStatement();
 
-            ResultSet rset = statement.executeQuery("select FirstName, LastName from person");
+            // ResultSet rset = statement.executeQuery("select FirstName, LastName from person");
 
-            while (rset.next()) {
+            ResultSet rset = statement.executeQuery("select firstname, lastname, age, city " +
+                    "from person where city is not null;");
 
-                String fName = rset.getString("FirstName");
-                String lName = rset.getString("LastName");
-                // System.out.println(fName + " " + lName);
-            }
+            // while (rset.next()) {
+            // String fName = rset.getString("FirstName");
+            // String lName = rset.getString("LastName");
+            // System.out.println(fName + " " + lName);
+            // }
 
             // ResultSetMetaData - object that contains information about the result
             // information such as in the table how many columns are there,
             // name of the columns, rows and number of the rows
+            ResultSetMetaData metaData = rset.getMetaData();
 
+            // print all the columns header values
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                String columnName = metaData.getColumnName(i);
+                System.out.println(columnName);
+            }
 
+            // we want to loop through every column and every row
+            while (rset.next()) {
+                for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                    String value = rset.getString(metaData.getColumnName(i));
+                    System.out.println(value + " ");
+                }
+
+                // just to change the line added below
+                System.out.println();
+            }
 
         } catch (SQLException e) {
             e.printStackTrace();
