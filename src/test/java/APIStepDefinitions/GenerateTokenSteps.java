391f3125b587cc0610c5b44aef999507404bfb8c
EvgenAleks <evgenii.aleksandroff@gmail.com>
2023-05-28 00:53:54 +0200

    api workflow added

--- src/test/java/APIStepDefinitions/GenerateTokenSteps.java
+++ src/test/java/APIStepDefinitions/GenerateTokenSteps.java
@@ -1,11 +1,32 @@
 package APIStepDefinitions;
 
 import io.cucumber.java.en.Given;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.given;
 
 public class GenerateTokenSteps {
 
+    String baseURI = RestAssured.baseURI = "http://hrm.syntaxtechs.net/syntaxapi/api";
+    public static String token;
+
     @Given("JWT is generated")
     public void jwt_is_generated() {
+        RequestSpecification generateTokenRequest =
+                given().header("Content-Type", "application/json").
+                        body("{\n" +
+                                "  \"email\": \"shakeit@icloud.com\",\n" +
+                                "  \"password\": \"test123\"\n" +
+                                "}");
+
+        // hit the endpoint
+        Response response = generateTokenRequest.when().post("/generateToken.php");
+
+        // store the token as global variable
+        token = "Bearer " + response.jsonPath().getString("token");
+        System.out.println(token);
 
     }
 }
--- src/test/resources/Features/APIworkflow.feature
+++ src/test/resources/Features/APIworkflow.feature
@@ -1,5 +1,10 @@
 Feature: API workflow for HRMS
 
+  Background:
+    Given JWT is generated
+
   @api
   Scenario: Create an employee using API call
-    Given JWT is generated
+    Given Request is prepared to create an employee
+    When POST call is made to create an employee
+    Then The status code for creating an employee is 201
