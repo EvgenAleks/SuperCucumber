4d2a6888f90a06f8f596c24f0980db2de322eacf
EvgenAleks <evgenii.aleksandroff@gmail.com>
2023-05-28 00:26:52 +0200

    api runner added

---
+++ src/test/java/APIStepDefinitions/GenerateTokenSteps.java
@@ -0,0 +1,11 @@
+package APIStepDefinitions;
+
+import io.cucumber.java.en.Given;
+
+public class GenerateTokenSteps {
+
+    @Given("JWT is generated")
+    public void jwt_is_generated() {
+
+    }
+}
---
+++ src/test/resources/Features/APIworkflow.feature
@@ -0,0 +1,5 @@
+Feature: API workflow for HRMS
+
+  @api
+  Scenario: Create an employee using API call
+    Given JWT is generated
